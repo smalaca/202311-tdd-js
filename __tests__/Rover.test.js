e2ca0aba00da1413279c128e251232123594a4a5
Fabian Grajko <fabian.g@masterborn.com>
2023-11-13 14:58:55 +0100

    REFACT add more tests

--- __tests__/Rover.test.js
+++ __tests__/Rover.test.js
@@ -21,7 +21,7 @@ describe("Rover", () => {
 
             rover.execute("F");
 
-            expect(rover.actualPosition()).toStrictEqual({ x: 1, y: 0});
+            expect(rover.actualPosition()).toStrictEqual({ x: 1, y: 0 });
         });
 
         test("should rotate right and move forward", () => {
@@ -29,7 +29,7 @@ describe("Rover", () => {
 
             rover.execute("RF");
 
-            expect(rover.actualPosition()).toStrictEqual({ x: 0, y: 1});
+            expect(rover.actualPosition()).toStrictEqual({ x: 0, y: 1 });
         });
 
         test("should rotate left and move forward", () => {
@@ -37,7 +37,23 @@ describe("Rover", () => {
 
             rover.execute("LF");
 
-            expect(rover.actualPosition()).toStrictEqual({ x: 0, y: 100});
+            expect(rover.actualPosition()).toStrictEqual({ x: 0, y: 100 });
+        });
+
+        test("should turn around and move forward", () => {
+            let rover = new Rover(0, 0);
+
+            rover.execute("RRF");
+
+            expect(rover.actualPosition()).toStrictEqual({ x: 100, y: 0 });
+        });
+
+        test("should rotate and move multiple times", () => {
+            let rover = new Rover(0, 0);
+
+            rover.execute("FLFFRF");
+
+            expect(rover.actualPosition()).toStrictEqual({ x: 2, y: 99 });
         });
     });
     
