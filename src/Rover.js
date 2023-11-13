2fcc6ed7bb6225c3836051b5953c3b255c5ed439
Fabian Grajko <fabian.g@masterborn.com>
2023-11-13 14:48:19 +0100

    REFACT add calculating position

--- src/Rover.js
+++ src/Rover.js
@@ -18,15 +18,64 @@ class Rover {
     }
 
     execute(command) {
-        if (command === "F") {
-            this.x = 1;
-            return;
+        command.split("").forEach((letter) => {
+            if (letter === "F"){
+                this.moveForward();
+            }
+
+            if (letter === "R") {
+                this.rotateRight();
+            }
+
+            if (letter === "L") {
+                this.rotateLeft();
+            }
+        });
+    }
+
+    moveForward() {
+        this.changePosition();
+        this.recalibratePosition();
+    }
+
+    changePosition() {
+        if (this.direction === RIGHT) {
+            this.x += 1;
         }
-        if (command === "RF") {
-            this.y = 1;
-            return;
+        if (this.direction === DOWN) {
+            this.y += 1;
         }
-        this.y = 100;
+        if (this.direction === LEFT) {
+            this.x -= 1;
+        }
+        if (this.direction === UP) {
+            this.y -= 1;
+        }
+    }
+
+    recalibratePosition() {
+        if (this.y < 0) {
+            this.y = 100;
+        }
+        if (this.y > 100) {
+            this.y = 0;
+        }
+
+        if (this.x < 0) {
+            this.x = 100;
+        }
+        if (this.x > 100) {
+            this.x = 0;
+        }
+    }
+
+    rotateRight() {
+        this.direction = (this.direction + 1) % 4;
+    }
+
+    rotateLeft() {
+        const newDirection = this.direction - 1;
+        this.direction = newDirection < 0 ? UP : newDirection;
     }
 }
 
