1ed7aea7b5c55c97a07d7267a3f90626ced707d6
smalaca <ab13.krakow@gmail.com>
2023-11-13 13:54:14 +0100

    RED - should recognize invalid x value

--- __tests__/Rover.test.js
+++ __tests__/Rover.test.js
@@ -1,9 +1,17 @@
 const Rover = require("../src/Rover");
 
 describe("Rover", () => {
-    test("should know its position", () => {
-        let rover = new Rover(0, 0);
+    describe("landing", () => {
+        test("should know its position", () => {
+            let rover = new Rover(0, 0);
 
-        expect(rover.actualPosition()).toStrictEqual({ x: 0, y: 0});
+            expect(rover.actualPosition()).toStrictEqual({ x: 0, y: 0});
+        });
+
+        test("should throw Error when invalid x given", () => {
+            let actual = () => new Rover(-5, 0);
+
+            expect(actual).toThrowError(new Error("Invalid x: -5"));
+        })
     });
 });
