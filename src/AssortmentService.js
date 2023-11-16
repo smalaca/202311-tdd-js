64ca09eaf7326b69a839b10828933f44af07011d
Filip Bachul <filip.b@masterborn.com>
2023-11-16 14:02:01 +0100

    GREEN - should not allow to add product with invalid name

--- src/AssortmentService.js
+++ src/AssortmentService.js
@@ -5,7 +5,7 @@ class AssortmentService {
 
     addProduct(dto) {
         if (!dto.code || typeof dto.code !== 'string') throw new Error("Invalid product code");
-        if (!dto.name) throw new Error("Invalid product name");
+        if (!dto.name || typeof dto.name !== 'string') throw new Error("Invalid product name");
         if (!dto.price || typeof dto.price !== 'number') throw new Error("Invalid product price");
         this.shopClient.addProduct(dto);
     }
