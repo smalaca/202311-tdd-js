d64530c6463c89168d534c13b91829c25d695fd3
Mateusz Russak <mateusz.r@masterborn.com>
2023-11-16 15:55:09 +0100

    feat: ids and events

--- __tests__/AssortmentService.test.js
+++ __tests__/AssortmentService.test.js
@@ -1,31 +1,61 @@
+class FieldError extends Error {
+  constructor(field, message) {
+    super(message);
+    this.field = field;
+  }
+}
 
 const validProduct = {
   code: "00000-000000-00000-00000-00000",
   name: "Pietruszka",
   price: 2.5,
-  description: "zielony"
-}
+  description: "zielony",
+};
 const backendApi = {
   addProduct: jest.fn(),
+};
+const eventEmitter = {
+  emit: jest.fn(),
+};
 
-}
-
-const createAssortmentService = (api) => {
+const createAssortmentService = (api, eventBus) => {
+  const reportError = (field, message) => {
+    throw new FieldError(field, message);
+  };
   const addProduct = (dto) => {
-    if (!dto.code || !dto.name || !dto.price)
-      throw new Error();
-    if(!dto.code.match(/^[\d-]{30}$/))
-      throw new Error();
-    if(!dto.name.match(/^.{5,50}$/))
-      throw new Error();
-    //other mocked operations
-    api.addProduct(dto);
-  }
+    try {
+      if (!dto.code) reportError("code", "Missing value");
+      if (!dto.name) reportError("name", "Missing value");
+      if (!dto.price) reportError("price", "Missing value");
+      if (!dto.code.match(/^[\d-]{30}$/))
+        reportError(
+          "code",
+          "value should be 30 characters long and contain only digits and dashes",
+        );
+      if (!dto.name.match(/^.{5,50}$/))
+        reportError("name", "value length should be between 5 and 50");
+      //other mocked operations
+      const id = api.addProduct(dto);
+      if (eventBus) {
+        eventBus.emit("productAdded", {id, ...dto});
+      }
+    } catch (e) {
+      if (eventBus) {
+        eventBus.emit("productAddingFailed", { [e.field]: e.message });
+      } else {
+        throw e;
+      }
+    }
+  };
   return { addProduct };
-}
+};
 
 describe("AssortmentService", () => {
-  describe('Basic adding', () => {
+  afterEach(() => {
+    eventEmitter.emit.mockClear();
+  });
+
+  describe("Basic adding", () => {
     it("Should add product", () => {
       const givenDTO = validProduct;
       const assortmentService = createAssortmentService(backendApi);
@@ -33,21 +63,21 @@ describe("AssortmentService", () => {
 
       expect(backendApi.addProduct).toHaveBeenCalledWith(givenDTO);
     });
-    it('product should have code', () => {
+    it("product should have code", () => {
       const givenDTO = { ...validProduct, code: undefined };
       const assortmentService = createAssortmentService(backendApi);
       const addProcess = () => assortmentService.addProduct(givenDTO);
 
       expect(addProcess).toThrow(Error);
     });
-    it('product should have name', () => {
+    it("product should have name", () => {
       const givenDTO = { ...validProduct, name: undefined };
       const assortmentService = createAssortmentService(backendApi);
       const addProcess = () => assortmentService.addProduct(givenDTO);
 
       expect(addProcess).toThrow(Error);
     });
-    it('product should have price', () => {
+    it("product should have price", () => {
       const givenDTO = { ...validProduct, price: undefined };
       const assortmentService = createAssortmentService(backendApi);
       const addProcess = () => assortmentService.addProduct(givenDTO);
@@ -61,48 +91,82 @@ describe("AssortmentService", () => {
 
       expect(backendApi.addProduct).toHaveBeenCalledWith(givenDTO);
     });
-  })
-  describe('code validation', () => {
-    it('Should have no more than 30 characters', () => {
-      const givenDTO = { ...validProduct, code: Array(31).fill('1').join("") };
+  });
+  describe("code validation", () => {
+    it("Should have no more than 30 characters", () => {
+      const givenDTO = { ...validProduct, code: Array(31).fill("1").join("") };
       const assortmentService = createAssortmentService(backendApi);
       const addProcess = () => assortmentService.addProduct(givenDTO);
 
       expect(addProcess).toThrow(Error);
-    })
-    it('Should have no less than 30 characters', () => {
-      const givenDTO = { ...validProduct, code: Array(29).fill('1').join("") };
+    });
+    it("Should have no less than 30 characters", () => {
+      const givenDTO = { ...validProduct, code: Array(29).fill("1").join("") };
       const assortmentService = createAssortmentService(backendApi);
       const addProcess = () => assortmentService.addProduct(givenDTO);
 
       expect(addProcess).toThrow(Error);
-    })
-    it('Should not allow invalid characters', () => {
-      const givenDTO = { ...validProduct, code: Array(30).fill('a').join("") };
+    });
+    it("Should not allow invalid characters", () => {
+      const givenDTO = { ...validProduct, code: Array(30).fill("a").join("") };
       const assortmentService = createAssortmentService(backendApi);
       const addProcess = () => assortmentService.addProduct(givenDTO);
 
       expect(addProcess).toThrow(Error);
-    })
-  })
-  describe('name validation', ()=> {
-    it('Should have no more than 50 characters', () => {
-      const givenDTO = { ...validProduct, name: Array(51).fill('1').join("") };
+    });
+  });
+  describe("name validation", () => {
+    it("Should have no more than 50 characters", () => {
+      const givenDTO = { ...validProduct, name: Array(51).fill("1").join("") };
       const assortmentService = createAssortmentService(backendApi);
       const addProcess = () => assortmentService.addProduct(givenDTO);
 
       expect(addProcess).toThrow(Error);
-    })
-    it('Should have no less than 5 characters', () => {
-      const givenDTO = { ...validProduct, name: Array(4).fill('1').join("") };
+    });
+    it("Should have no less than 5 characters", () => {
+      const givenDTO = { ...validProduct, name: Array(4).fill("1").join("") };
       const assortmentService = createAssortmentService(backendApi);
       const addProcess = () => assortmentService.addProduct(givenDTO);
 
       expect(addProcess).toThrow(Error);
-    })
-  })
-  describe('price validation', ()=> {
-    
-  })
+    });
+  });
+  describe("Events", () => {
+    it("Should emit event on success", () => {
+      const givenDTO = validProduct;
+      const assortmentService = createAssortmentService(
+        backendApi,
+        eventEmitter,
+      );
+      assortmentService.addProduct(givenDTO);
 
+      expect(eventEmitter.emit).toHaveBeenCalledWith("productAdded", givenDTO);
+    });
+    it("should publish errors on event bus", () => {
+      const givenDTO = { ...validProduct, code: undefined };
+      const assortmentService = createAssortmentService(
+        backendApi,
+        eventEmitter,
+      );
+      assortmentService.addProduct(givenDTO);
+      expect(eventEmitter.emit).toHaveBeenCalledWith("productAddingFailed", {
+        code: "Missing value",
+      });
+    });
+  });
+  describe("Ids", () => {
+    it("should generate id", () => {
+      const givenDTO = validProduct;
+      const backendApi = {
+        addProduct: () => "id",
+      };
+      const assortmentService = createAssortmentService(backendApi, eventEmitter);
+      assortmentService.addProduct(givenDTO);
+
+      expect(eventEmitter.emit).toHaveBeenCalledWith("productAdded", {
+        ...givenDTO,
+        id: 'id',
+      });
+    });
+  });
 });
