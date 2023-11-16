25467821fc7b19423037f000ab3e931e082304a9
Fabian Grajko <fabian.g@masterborn.com>
2023-11-16 14:11:25 +0100

    GREEN should not allow to add product with code length different than 30 characters

--- src/AssortmentService.js
+++ src/AssortmentService.js
@@ -4,7 +4,7 @@ class AssortmentService {
     }
 
     addProduct(dto) {
-        if (!dto.code || typeof dto.code !== 'string') throw new Error("Invalid product code");
+        if (!dto.code || typeof dto.code !== 'string' || dto.code.length !== 30) throw new Error("Invalid product code");
         if (!dto.name || typeof dto.name !== 'string') throw new Error("Invalid product name");
         if (!dto.price || typeof dto.price !== 'number') throw new Error("Invalid product price");
         if (dto.description && typeof dto.description !== 'string') throw new Error("Invalid product description");
