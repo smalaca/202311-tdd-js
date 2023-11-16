e6ea9573725180a2defc8d3717dfd4860e19c771
PawelRugalaMB <pawel.r@masterborn.com>
2023-11-16 13:36:22 +0100

    RED phase

--- __tests__/AssortmentService.test.js
+++ __tests__/AssortmentService.test.js
@@ -1,15 +1,20 @@
 const AssortmentService = require('../src/Assortment.service');
+const shopClient = {
+    addProduct: jest.fn()
+};
 
 describe("AssortmentService", () => {
     test("should allow add product", () => {
         const dto = VALID_PRODUCT;
-        const shopClient = {
-            addProduct: jest.fn()
-        };
 
         new AssortmentService(shopClient).addProduct(dto);
 
         expect(shopClient.addProduct).toHaveBeenCalledWith(dto);
+    })
+    test("should throw when required fields are not present", () => {
+        const dto = INVALID_PRODUCT;
+
+        expect(() => new AssortmentService(shopClient).addProduct(dto)).toThrow("Invalid Product")
     })
 })
 
@@ -17,4 +22,9 @@ const VALID_PRODUCT = {
     name: "book",
     code: "some code",
     price: 123.45,
+}
+
+const INVALID_PRODUCT = {
+    code: "some code",
+    price: 123.45, 
 }
