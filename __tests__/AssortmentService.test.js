a4f88566c57beee9f882fe42b78fb7fcaff5c08f
smalaca <ab13.krakow@gmail.com>
2023-11-16 13:23:57 +0100

    RED - should allow add product

---
+++ __tests__/AssortmentService.test.js
@@ -0,0 +1,22 @@
+const AssortmentService = require("./AssortmentService");
+
+describe("AssortmentService", () => {
+    test("should allow add product", () => {
+        let dto = givenValidProductDto();
+        let shopClient = {
+            addProduct: jest.fn()
+        };
+
+        new AssortmentService(shopClient).addProduct(dto);
+
+        expect(shopClient.addProduct).toHaveBeenCalledWith(dto);
+    });
+
+    function givenValidProductDto() {
+        return {
+            name: "book",
+            code: "some code",
+            price: 123.45
+        };
+    }
+})
