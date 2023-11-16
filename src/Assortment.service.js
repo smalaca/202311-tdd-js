fa2d4b84b9ec3653c665d8c09177d13ec431c56a
PawelRugalaMB <pawel.r@masterborn.com>
2023-11-16 15:38:58 +0100

    GREEN

--- src/Assortment.service.js
+++ src/Assortment.service.js
@@ -1,15 +1,19 @@
 const INVALID = require('./Errors')
 const AssortmentValidator = require('./Assortment.controller');
 class AssortmentService {
+    #productList = []
     constructor(shopClient) {
         this.shopClient = shopClient;
     }
 
-
+    getProducts() {
+        return this.#productList;
+    }
 
     addProduct(dto, amount) {
         AssortmentValidator.validateProduct(dto);
         this.shopClient.addProduct(dto);
+        this.#productList.push(dto);
     }
 }
 
