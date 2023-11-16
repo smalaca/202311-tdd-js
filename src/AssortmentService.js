6128e4f1ad59ea8c6db0d385c3389fcd7fa31d60
Filip Bachul <filip.b@masterborn.com>
2023-11-16 14:05:26 +0100

    GREEN - should not allow to add product with invalid description

--- src/AssortmentService.js
+++ src/AssortmentService.js
@@ -7,6 +7,7 @@ class AssortmentService {
         if (!dto.code || typeof dto.code !== 'string') throw new Error("Invalid product code");
         if (!dto.name || typeof dto.name !== 'string') throw new Error("Invalid product name");
         if (!dto.price || typeof dto.price !== 'number') throw new Error("Invalid product price");
+        if (dto.description && typeof dto.description !== 'string') throw new Error("Invalid product description");
         this.shopClient.addProduct(dto);
     }
 }
