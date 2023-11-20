6cb6ef363140f83676f0b50d545d6f888e012bd2
smalaca <ab13.krakow@gmail.com>
2023-11-20 23:20:49 +0100

    REFACT - merge describe and test into one test

--- __tests__/ProductCodeFactory.test.js
+++ __tests__/ProductCodeFactory.test.js
@@ -3,12 +3,10 @@ const ProductCodeFactory = require("../src/ProductCodeFactory")
 describe('ProductCodeFactory', () => {
     const factory = new ProductCodeFactory();
 
-    describe('should start with product name', () => {
-        test('when product contains only alphanumeric characters and have 15 character length', () => {
-            let actual = factory.create("1t15Pr0ductN4m3");
+    test('should start with name when contains only alphanumeric characters and have 15 character length', () => {
+        let actual = factory.create("1t15Pr0ductN4m3");
 
-            expect(actual.startsWith("1t15Pr0ductN4m3")).toBeTruthy();
-        });
+        expect(actual.startsWith("1t15Pr0ductN4m3")).toBeTruthy();
     });
 
 })
