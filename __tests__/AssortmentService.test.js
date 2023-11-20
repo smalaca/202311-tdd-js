63ec27e44b32486a78c0e22ab7ecb74e49f8b2b8
smalaca <ab13.krakow@gmail.com>
2023-11-20 20:47:04 +0100

    REFACT - should not add product when all values are invalid

--- __tests__/AssortmentService.test.js
+++ __tests__/AssortmentService.test.js
@@ -290,6 +290,23 @@ describe("AssortmentService", () => {
             expect(actual.getErrors()).toContainEqual(new ValidationError("price", "Missing product price"));
             expect(actual.getErrors()).toContainEqual(new ValidationError("amount", "Missing product amount"));
         })
+
+        test('when all values are invalid', () => {
+            let command = new AddProductCommand(NO_VALUE, 0, "aaaa", "ABC", 0);
+
+            assortmentService.addProduct(command);
+
+            expect(shopClient.addProduct).not.toHaveBeenCalled();
+            expect(eventPublisher.publish).toHaveBeenCalled();
+            let actual = eventPublisher.publish.mock.calls[0][0];
+            expect(actual.constructor.name).toBe("ProductCouldNotBeAdded");
+            expect(actual.getErrors()).toHaveLength(5);
+            expect(actual.getErrors()).toContainEqual(new ValidationError("assortmentId", "Missing assortment id"));
+            expect(actual.getErrors()).toContainEqual(new ValidationError("name", "Invalid product name"));
+            expect(actual.getErrors()).toContainEqual(new ValidationError("code", "Invalid product code"));
+            expect(actual.getErrors()).toContainEqual(new ValidationError("price", "Invalid product price"));
+            expect(actual.getErrors()).toContainEqual(new ValidationError("amount", "Invalid product amount"));
+        })
     });
 
     test('should publish ProductCouldNotBeAdded event when product could not be added', () => {
