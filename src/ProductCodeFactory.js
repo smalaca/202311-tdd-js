b683c8027bbf0785def1096cbdfb258d993a024d
smalaca <ab13.krakow@gmail.com>
2023-11-20 23:37:47 +0100

    GREEN - should have 30 characters when name has less than 15 characters

--- src/ProductCodeFactory.js
+++ src/ProductCodeFactory.js
@@ -1,7 +1,11 @@
 class ProductCodeFactory {
+    static #CODE_LENGTH = 30;
 
     create(name) {
-        return name.replaceAll(' ', '-').substring(0, 15).replace(/[^a-zA-Z0-9-]/g, "") + "XXXXXXXXXXXXXXX";
+        let fromName = name.replaceAll(' ', '-').substring(0, 15).replace(/[^a-zA-Z0-9-]/g, "");
+        let leftToFill = "X".repeat(ProductCodeFactory.#CODE_LENGTH - fromName.length);
+
+        return fromName + leftToFill;
     }
 }
 
