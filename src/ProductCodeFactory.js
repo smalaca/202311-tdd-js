e8a3b100f5227cecb86bb60ac44da58c9bb3b730
smalaca <ab13.krakow@gmail.com>
2023-11-20 23:46:12 +0100

    GREEN - should code ends with random alphanumeric characters

--- src/ProductCodeFactory.js
+++ src/ProductCodeFactory.js
@@ -3,10 +3,19 @@ class ProductCodeFactory {
 
     create(name) {
         let fromName = name.replaceAll(' ', '-').substring(0, 15).replace(/[^a-zA-Z0-9-]/g, "");
-        let leftToFill = "X".repeat(ProductCodeFactory.#CODE_LENGTH - fromName.length);
+        let leftToFill = this.#randomAlphaNumeric(ProductCodeFactory.#CODE_LENGTH - fromName.length);
 
         return fromName + leftToFill;
     }
+
+    #randomAlphaNumeric(length) {
+        let s = '';
+        Array.from({ length }).some(() => {
+            s += Math.random().toString(36).slice(2);
+            return s.length >= length;
+        });
+        return s.slice(0, length);
+    };
 }
 
 module.exports = ProductCodeFactory;
